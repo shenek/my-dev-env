0f2480843f22156b6073525bd9b4d2ac16561d12
Stepan Henek <stepan.henek@nic.cz>
2019-10-10 15:59:45 +0200

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ used for development in python.\
 
 setup(
     name="my-dev-env",
-    version="0.1",
+    version="0.2",
     author="Stepan Henek",
     author_email="stepan@henek.name",
     url="https://github.com/shenek/my-dev-env",
