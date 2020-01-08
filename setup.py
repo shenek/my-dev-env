444ca4e0c86b12d1ed1f4c4e4d3a518603859de8
Stepan Henek <stepan@henek.name>
2020-01-08 23:18:54 +0100

    adding isort

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     description=DESCRIPTION,
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=["black", "python-language-server", "ipython", "ipdb", "mypy"],
+    install_requires=["black", "python-language-server", "ipython", "ipdb", "mypy", "isort"],
     extras_require={
         "node": ["nodeenv"],
         "django": ["my-dev-env[node]", "django-extensions", "django-debug-toolbar"],
